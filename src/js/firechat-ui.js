2433eb52ffe85793ab8dac42f78d040cca5c23b2
pandabearcoder <vlouie00@gmail.com>
2018-03-28 10:01:50 +0000

    Updated message timestamp

--- src/js/firechat-ui.js
+++ src/js/firechat-ui.js
@@ -1098,8 +1098,13 @@
         day = date.getDate(),
         hours = date.getHours() || 12,
         minutes = '' + date.getMinutes(),
+        seconds = date.getSeconds(),
         ampm = (date.getHours() >= 12) ? 'pm' : 'am';
 
+    // 2018-01-03 16:32:00
+    return (year + '-' + month + '-' + day + ' ' + hours + ':' + minutes + ':' + seconds);
+
+    /* Old format
     var monthWords = [
       'Jan', 'Feb', 'Mar',
       'Apr', 'May', 'Jun',
@@ -1111,7 +1116,9 @@
 
     hours = (hours > 12) ? hours - 12 : hours;
     minutes = (minutes.length < 2) ? '0' + minutes : minutes;
+    // 03 Jan, 2018 04:32pm
     return (day + ' ' + currentMonth + ', ' + year + ' ' + hours + ':' + minutes + ampm);
+    */
   };
 
   /**
